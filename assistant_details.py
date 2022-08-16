5a9faec7e1a3f9319323aa727fc62c463350944b
mahadie02 <mahadie02@gmail.com>
2022-08-16 17:47:51 +0600

    first commit

--- assistant_details.py
+++
@@ -1,7 +0,0 @@
-#_____________Imports____________
-from database_module import get_asd
-
-#____Retrieving Assistant Name_____
-name = get_asd("assistant_name")
-voice_model = get_asd("voice model")
-current_wp = get_asd("current_wp")
